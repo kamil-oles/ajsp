00b967efd24af5bc7c977c30288d3c478ebe04d8
Kamil Olesiński <oles.kamil@gmail.com>
2018-09-14 19:56:44 +0200

    Current rates: data preparation for display

--- src/app/components/rates/rates-current/rates-current.module.js
+++ src/app/components/rates/rates-current/rates-current.module.js
@@ -1,18 +1,22 @@
 import angular from 'angular';
 import uiRouter from '@uirouter/angularjs';
+import { RatesCurrentHttpService } from './rates-current-http.service';
 import { ratesCurrentComponent } from './rates-current.component';
 
 export const appRatesCurrent = angular
   .module('appRatesCurrent', [uiRouter])
   .component('appRatesCurrent', ratesCurrentComponent)
+  .service('RatesCurrentHttpService', RatesCurrentHttpService)
   .config($stateProvider => {
     $stateProvider
       .state('appRates.current', {
         url: '/current',
         component: 'appRatesCurrent',
         resolve: {
-          currentRates: function (ComponentsHttpService) {
-            return ComponentsHttpService.currentRates().then(response => response.data[0]);
+          currentRates: function (RatesCurrentHttpService) {
+            return RatesCurrentHttpService.currentRates().then(response => {
+              return RatesCurrentHttpService.prepareRates(response.data[0]);
+            });
           }
         }
       });
