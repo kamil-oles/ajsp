fc655f1e3413690c57902a2c5abdfe30fbd09e4b
Kamil Olesiński <oles.kamil@gmail.com>
2018-11-05 23:55:30 +0100

    Rates component: active state detection

--- src/app/components/rates/rates.component.js
+++ src/app/components/rates/rates.component.js
@@ -1,3 +1,23 @@
 export const ratesComponent = {
-  template: require('./rates.html')
+  template: require('./rates.html'),
+  controller: class RatesComponentController {
+    constructor($state) {
+      this.state = $state;
+    }
+
+    $onInit() {
+      this.setState();
+    }
+
+    navigate(state) {
+      this.state.go(state).then(() => {
+        this.setState();
+      });
+    }
+
+    setState() {
+      this.current = this.state.current.name === 'appRates.current' ? true : false;
+      this.historical = !this.current;
+    }
+  }
 };
