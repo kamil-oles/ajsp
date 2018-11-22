41c96c7ec92732a77b8ee92539a8d6654ac7cd0c
Kamil Olesiński <oles.kamil@gmail.com>
2018-11-22 17:43:56 +0100

    Toast directive: regex

--- src/app/common/directives/toast.directive.js
+++ src/app/common/directives/toast.directive.js
@@ -7,12 +7,14 @@ export class ToastDirective {
 
   link(scope, element, attrs, controller) {
     scope.$parent.$on('toast', (event, message) => {
+      const messageProcessed = controller.processMessage(message);
       controller.toast.show(
         controller.toast.simple()
+          .action('Zamknij')
           .hideDelay(false)
           .parent(element)
           .position('top, right')
-          .textContent(message)
+          .textContent(messageProcessed)
       );
       event.stopPropagation();
     });
@@ -22,5 +24,16 @@ export class ToastDirective {
 class ToastDirectiveController {
   constructor($mdToast) {
     this.toast = $mdToast;
+  }
+
+  regex = /-\s(.*)\s\//;
+
+  processMessage(message) {
+    const result = this.regex.exec(message);
+    if (Array.isArray(result)) {
+      return result[1];
+    } else {
+      return '';
+    }
   }
 }
