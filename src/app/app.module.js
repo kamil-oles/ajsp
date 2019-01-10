6ccc8d526354cac9303f36dbe155e38374e087c0
Kamil Olesiński <oles.kamil@gmail.com>
2019-01-10 08:38:54 +0100

    Theme implementation

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -12,8 +12,28 @@ import './app.scss';
 export const app = angular
   .module('app', ['ngMessages', 'ngMaterial', uiRouter, appCommon, appComponents])
   .component('app', appComponent)
-  .config(($locationProvider, $urlRouterProvider) => {
+  .config(($locationProvider, $mdThemingProvider, $urlRouterProvider) => {
     $locationProvider.html5Mode(true);
     $urlRouterProvider.otherwise('/converter');
+    $mdThemingProvider.definePalette('ultra_violet', {
+      '50': 'ECEAF1',
+      '100': 'CFCADD',
+      '200': 'B0A6C6',
+      '300': '9082AF',
+      '400': '78689E',
+      '500': '604D8D',
+      '600': '584685',
+      '700': '4E3D7A',
+      '800': '443470',
+      '900': '33255D',
+      'A100': 'B8A1FF',
+      'A200': '916EFF',
+      'A400': '6B3BFF',
+      'A700': '5722FF',
+      'contrastDefaultColor': 'light',
+      'contrastDarkColors': ['50', '100', '200', '300', '400', 'A100'],
+      'contrastLightColors': undefined
+    });
+    $mdThemingProvider.theme('default').primaryPalette('ultra_violet');
   })
   .name;
