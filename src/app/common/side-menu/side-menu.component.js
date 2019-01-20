0f2e476f54a29c18d1c0f26c7ce5162999369557
Kamil Olesiński <oles.kamil@gmail.com>
2019-01-20 12:25:06 +0100

    Side menu component: submenu

--- src/app/common/side-menu/side-menu.component.js
+++ src/app/common/side-menu/side-menu.component.js
@@ -1,3 +1,10 @@
 export const SIDE_MENU_COMPONENT = {
-  template: require('./side-menu.html')
+  template: require('./side-menu.html'),
+  controller: class SideMenuComponentController {
+    subMenu = false;
+
+    showSubMenu() {
+      this.subMenu = !this.subMenu;
+    }
+  }
 };
