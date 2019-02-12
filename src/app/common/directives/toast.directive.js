c042bb19b02ab88f299c3780ed3f692725b25549
Kamil Olesiński <oles.kamil@gmail.com>
2019-02-12 18:54:24 +0100

    Toast refactor

--- src/app/common/directives/toast.directive.js
+++ src/app/common/directives/toast.directive.js
@@ -1,26 +1,26 @@
 export class ToastDirective {
   constructor() {
-    this.controller = ToastDirectiveController;
+    this.controller = ToastDirectiveCtrl;
     this.restrict = 'A';
     this.scope = {};
   }
 
-  link(scope, element, attrs, controller) {
-    scope.$parent.$on('toast', (event, message) => {
-      const messageProcessed = controller.processMessage(message);
+  link(scope, element, attrs, ctrl) {
+    scope.$parent.$on('toast', function showToast(event, message) {
+      const MESSAGE_PROCESSED = ctrl.processMessage(message);
       if (element.hasClass('common-toast-hide')) {
         element.toggleClass('common-toast-show common-toast-hide');
       }
-      controller.toast.show(
-        controller.toast.simple()
+      ctrl.toast.show(
+        ctrl.toast.simple()
           .action('ZAMKNIJ')
           .hideDelay(false)
           .parent(element)
           .position('top right')
-          .textContent(messageProcessed)
-      ).then(() => {
+          .textContent(MESSAGE_PROCESSED)
+      ).then(function changeClass() {
         element.toggleClass('common-toast-show common-toast-hide');
-      }, () => {
+      }, function fallback() {
         return;
       });
       event.stopPropagation();
@@ -28,17 +28,24 @@ export class ToastDirective {
   }
 }
 
-class ToastDirectiveController {
-  constructor($mdToast) {
+class ToastDirectiveCtrl {
+  constructor($mdToast, $transitions) {
     this.toast = $mdToast;
+    this.transitions = $transitions;
   }
 
   regex = /-\s(.*)\s\//;
 
+  $onInit() {
+    this.transitions.onStart({}, () => {
+      this.toast.hide();
+    });
+  }
+
   processMessage(message) {
-    const result = this.regex.exec(message);
-    if (Array.isArray(result)) {
-      return result[1];
+    const RESULT = this.regex.exec(message);
+    if (Array.isArray(RESULT)) {
+      return RESULT[1];
     } else {
       return '';
     }
