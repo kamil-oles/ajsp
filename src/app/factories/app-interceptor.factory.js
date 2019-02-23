45c7f3cc1c9fced5c42a4095ae5e52690e5ee9e8
Kamil Olesiński <oles.kamil@gmail.com>
2019-02-23 13:14:23 +0100

    Interceptor prototype

--- src/app/factories/app-interceptor.factory.js
+++ src/app/factories/app-interceptor.factory.js
@@ -1,8 +1,35 @@
-export const APP_INTERCEPTOR_FACTORY = function () {
+export const APP_INTERCEPTOR_FACTORY = function ($interval, $timeout) {
+  let requestArray = [];
+
+  function clearArray(array, url) {
+    const INDEX = array.indexOf(url);
+    array.splice(INDEX, 1);
+    return array;
+  }
+
+  function showLoader(url) {
+    if (requestArray.indexOf(url) !== -1) {
+      console.log('---------LOADER!!!!');
+    }
+    const INTERVAL = $interval(function checkRequestArray() {
+      if (requestArray.length === 0) {
+        console.log('---------HIDE LOADER!!!');
+        $interval.cancel(INTERVAL);
+      }
+    }, 500);
+  }
+
   return {
     'request': function (config) {
-      console.log(config);
+      requestArray.push(config.url);
+      $timeout(function loader() {
+        showLoader(config.url);
+      }, 1500);
       return config;
+    },
+    'response': function (response) {
+      requestArray = clearArray(requestArray, response.config.url);
+      return response;
     }
   };
 };
