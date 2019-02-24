58f28df25874229e00135e863e44d197b9d72be5
Kamil Olesiński <oles.kamil@gmail.com>
2019-02-24 21:38:19 +0100

    Submit button component: loader

--- src/app/common/submit-button/submit-button.component.js
+++ src/app/common/submit-button/submit-button.component.js
@@ -1,18 +1,35 @@
 export const SUBMIT_BUTTON_COMPONENT = {
   bindings: {
     active: '<',
+    label: '<'
   },
   template: require('./submit-button.html'),
   controller: class SubmitButtonComponentCtrl {
-    constructor($scope) {
+    constructor($animate, $element, $scope) {
+      this.animate = $animate;
+      this.element = $element;
       this.scope = $scope;
     }
 
-    loading = false;
-
     $onInit() {
       this.scope.$on('loader', (event, loader) => {
-        this.loading = loader;
+        this.display(loader);
+      });
+    }
+
+    $postLink() {
+      const ELEMENTS = this.element.children().children();
+      this.labelWrapper = ELEMENTS[0];
+      this.loaderWrapper = ELEMENTS[1];
+    }
+
+    display(loader) {
+      const HIDE = loader ? this.labelWrapper : this.loaderWrapper,
+        SHOW = loader ? this.loaderWrapper : this.labelWrapper;
+      this.loading = loader;
+      this.animate.setClass(HIDE, 'submit-button-hide', 'submit-button-show').then(() => {
+        HIDE.className += ', submit-button-hidden';
+        this.animate.setClass(SHOW, 'submit-button-show', 'submit-button-hidden');
       });
     }
   }
--- src/app/common/submit-button/submit-button.module.js
+++ src/app/common/submit-button/submit-button.module.js
@@ -1,5 +1,6 @@
 import angular from 'angular';
 import { SUBMIT_BUTTON_COMPONENT } from './submit-button.component';
+import './submit-button.scss';
 
 export const APP_SUBMIT_BUTTON = angular
   .module('appSubmitButton', [])
