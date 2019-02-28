c44f8d4c67bf5d3d18169b75f8901192aab89d8a
Kamil Olesiński <oles.kamil@gmail.com>
2019-02-28 17:23:10 +0100

    Scroll top on state change

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -1,3 +1,15 @@
 export const APP_COMPONENT = {
-  template: require('./app.html')
+  template: require('./app.html'),
+  controller: class AppComponentCtrl {
+    constructor($transitions, $window) {
+      this.transitions = $transitions;
+      this.window = $window;
+    }
+
+    $onInit() {
+      this.transitions.onSuccess({}, () => {
+        this.window.scroll(0, 0);
+      });
+    }
+  }
 };
