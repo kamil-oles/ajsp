2feb89c18afca5502f00fc327457662eef2216ba
Kamil Olesiński <oles.kamil@gmail.com>
2019-03-19 20:00:35 +0100

    Submit button component: private properties and methods

--- src/app/common/submit-button/submit-button.component.js
+++ src/app/common/submit-button/submit-button.component.js
@@ -6,34 +6,33 @@ export const SUBMIT_BUTTON_COMPONENT = {
   },
   template: require('./submit-button.html'),
   controller: class SubmitButtonComponentCtrl {
-    constructor($animate, $element, $scope, $transitions) {
-      this.animate = $animate;
-      this.element = $element;
-      this.scope = $scope;
-      this.transitions = $transitions;
+    constructor($animate, $element, $scope) {
+      this._animate = $animate;
+      this._element = $element;
+      this._scope = $scope;
     }
 
     $onInit() {
-      this.scope.$on('loader', (event, loader) => {
+      this._scope.$on('loader', (event, loader) => {
         if (this.showLoader) {
-          this.display(loader);
+          this._display(loader);
         }
       });
     }
 
     $postLink() {
-      const ELEMENTS = this.element.children().children();
+      const ELEMENTS = this._element.children().children();
       this.labelWrapper = ELEMENTS[0];
       this.loaderWrapper = ELEMENTS[1];
     }
 
-    display(loader) {
+    _display(loader) {
       const HIDE = loader ? this.labelWrapper : this.loaderWrapper,
         SHOW = loader ? this.loaderWrapper : this.labelWrapper;
       this.loading = loader;
-      this.animate.setClass(HIDE, 'submit-button-hide', 'submit-button-show').then(() => {
+      this._animate.setClass(HIDE, 'submit-button-hide', 'submit-button-show').then(() => {
         HIDE.className += ', submit-button-hidden';
-        this.animate.setClass(SHOW, 'submit-button-show', 'submit-button-hidden');
+        this._animate.setClass(SHOW, 'submit-button-show', 'submit-button-hidden');
       });
     }
   }
