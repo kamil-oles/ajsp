1bd80df9bc5f9a049c2aa56025f7360bae379c98
Kamil Olesiński <oles.kamil@gmail.com>
2019-03-24 14:28:51 +0100

    Firebase removal

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -4,8 +4,6 @@ import 'angular-aria';
 import 'angular-messages';
 import 'angular-material';
 
-import firebase from 'firebase/app';
-import 'firebase/firestore';
 import uiRouter from '@uirouter/angularjs';
 
 import * as menu from './data/menu.json';
@@ -16,21 +14,12 @@ import { APP_INTERCEPTOR_FACTORY } from './factories/app-interceptor.factory';
 
 import './app.scss';
 
-firebase.initializeApp({
-  apiKey: 'AIzaSyB6Rm_TCkBZOx3iAV2QiAheSNPikmF1vZc',
-  authDomain: 'currency-converter-95682.firebaseapp.com',
-  projectId: 'currency-converter-95682'
-});
-
 angular
   .module('app', ['ngMessages', 'ngMaterial', uiRouter, APP_COMMON, appComponents])
   .component('app', APP_COMPONENT)
   .factory('appInterceptor', APP_INTERCEPTOR_FACTORY)
   .value('eventEmitter', function (payload) {
     return { $event: payload };
-  })
-  .value('firestore', function firestore() {
-    return firebase.firestore();
   })
   .value('menu', menu.data)
   .config(($httpProvider, $locationProvider, $mdThemingProvider, $urlRouterProvider) => {
--- src/app/components/components.module.js
+++ src/app/components/components.module.js
@@ -1,10 +1,8 @@
 import { APP_CONVERTER } from './converter/converter.module';
 import { APP_RATES } from './rates/rates.module';
-import { ComponentsDbService } from './services/components-db.service';
 
 export const appComponents = angular
   .module('appComponents', [APP_CONVERTER, APP_RATES])
-  .service('ComponentsDb', ComponentsDbService)
   .value('base', {
     currency: 'EUR',
     url: 'http://api.nbp.pl/api/exchangerates/'
