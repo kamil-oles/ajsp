d8fbd5c0267e553c5bc2fe5b3ead591b92d1240f
Kamil Olesiński <oles.kamil@gmail.com>
2019-04-14 22:00:46 +0200

    Service registration

--- src/app/components/charts/charts.module.js
+++ src/app/components/charts/charts.module.js
@@ -1,5 +1,6 @@
 import 'angular-chart.js';
 
+import { ChartsConfigService } from './services/charts-config.service';
 import { ChartsDataService } from './services/charts-data.service';
 import { ChartsHttpService } from './services/charts-http.service';
 import { CHARTS_COMPONENT } from './charts.component';
@@ -8,6 +9,7 @@ import './charts.scss';
 
 export const APP_CHARTS = angular
   .module('appCharts', ['chart.js'])
+  .service('ChartsConfig', ChartsConfigService)
   .service('ChartsData', ChartsDataService)
   .service('ChartsHttp', ChartsHttpService)
   .component('appCharts', CHARTS_COMPONENT)
