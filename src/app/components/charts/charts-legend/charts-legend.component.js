bf0effe7f044670c6d51150df8da363d821f51b7
Kamil Olesiński <oles.kamil@gmail.com>
2019-05-12 13:29:46 +0200

    Charts legend component: animations

--- src/app/components/charts/charts-legend/charts-legend.component.js
+++ src/app/components/charts/charts-legend/charts-legend.component.js
@@ -1,4 +1,13 @@
+class ChartsLegendComponentCtrl {
+  $onChanges(changes) {
+    if (changes.codes.currentValue[1].code) {
+      this.secondCode = changes.codes.currentValue[1].code;
+    }
+  }
+}
+
 export const CHARTS_LEGEND_COMPONENT = {
   bindings: { codes: '<' },
-  template: require('./charts-legend.html')
+  template: require('./charts-legend.html'),
+  controller: ChartsLegendComponentCtrl
 };
