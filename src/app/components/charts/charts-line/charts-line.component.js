08a4885065b04bd56855dfda051fc0ef98dba1e8
Kamil Olesiński <oles.kamil@gmail.com>
2019-05-22 19:12:36 +0200

    Charts component: orientation change - interval

--- src/app/components/charts/charts-line/charts-line.component.js
+++ src/app/components/charts/charts-line/charts-line.component.js
@@ -2,33 +2,21 @@ import Chart from 'chart.js';
 
 class ChartsLineComponentCtrl {
   /* @ngInject */
-  constructor($element, $window, ChartsLineConfig) {
+  constructor($element, $interval, $window, ChartsLineConfig) {
     this._config = ChartsLineConfig;
     this._element = $element;
+    this._interval = $interval;
     this._window = $window;
   }
 
   $onChanges(changes) {
     if (changes.data.currentValue[0].length) {
       this._prepareChart();
-      //   if (this._chart) {
-      //     this._chart.destroy();
-      //   }
-      //   this._chart = new Chart(this._ctx, {
-      //     data: {
-      //       datasets: this._config.dataset(this.data, this.codes),
-      //       labels: this.labels
-      //     },
-      //     options: this._config.options(),
-      //     type: 'line'
-      //   });
-      //   this.show = true;
-      // }
     }
   }
 
   $onInit() {
-    this._boundEventHandler = this._prepareChart;
+    this._boundEventHandler = this._orientationChangeHandler;
     this._window.addEventListener('orientationchange', this._boundEventHandler.bind(this));
   }
 
@@ -38,6 +26,9 @@ class ChartsLineComponentCtrl {
 
   $onDestroy() {
     this._window.removeEventListener('orientationchange', this.boundEventHandler);
+    if (this._intervalPromise) {
+      this._interval.cancel(this._intervalPromise);
+    }
   }
 
   _prepareChart() {
@@ -53,6 +44,24 @@ class ChartsLineComponentCtrl {
       type: 'line'
     });
     this.show = true;
+  }
+
+  _orientationChangeHandler() {
+    if (this.data[0].length) {
+      let noChangeCounter = 0;
+      const LAST_INNER_WIDTH = this._window.innerWidth;
+      this._intervalPromise = this._interval(() => {
+        if (LAST_INNER_WIDTH === this._window.innerWidth) {
+          noChangeCounter++;
+          if (noChangeCounter > 10) {
+            this._interval.cancel(this._intervalPromise);
+          }
+        } else {
+          this._prepareChart();
+          this._interval.cancel(this._intervalPromise);
+        }
+      }, 100);
+    }
   }
 }
 
