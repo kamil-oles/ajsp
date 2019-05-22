6ea034f5ab2eeef7f4d9650136699fa7d1d1e55e
Kamil Olesiński <oles.kamil@gmail.com>
2019-05-22 18:14:33 +0200

    Charts component: orientation change

--- src/app/components/charts/charts-line/charts-line.component.js
+++ src/app/components/charts/charts-line/charts-line.component.js
@@ -2,30 +2,57 @@ import Chart from 'chart.js';
 
 class ChartsLineComponentCtrl {
   /* @ngInject */
-  constructor($element, ChartsLineConfig) {
+  constructor($element, $window, ChartsLineConfig) {
     this._config = ChartsLineConfig;
     this._element = $element;
+    this._window = $window;
   }
 
   $onChanges(changes) {
     if (changes.data.currentValue[0].length) {
-      if (this._chart) {
-        this._chart.destroy();
-      }
-      this._chart = new Chart(this._ctx, {
-        data: {
-          datasets: this._config.dataset(this.data, this.codes),
-          labels: this.labels
-        },
-        options: this._config.options(),
-        type: 'line'
-      });
-      this.show = true;
+      this._prepareChart();
+      //   if (this._chart) {
+      //     this._chart.destroy();
+      //   }
+      //   this._chart = new Chart(this._ctx, {
+      //     data: {
+      //       datasets: this._config.dataset(this.data, this.codes),
+      //       labels: this.labels
+      //     },
+      //     options: this._config.options(),
+      //     type: 'line'
+      //   });
+      //   this.show = true;
+      // }
     }
+  }
+
+  $onInit() {
+    this._boundEventHandler = this._prepareChart;
+    this._window.addEventListener('orientationchange', this._boundEventHandler.bind(this));
   }
 
   $postLink() {
     this._ctx = this._element.children().children()[0].getContext('2d');
+  }
+
+  $onDestroy() {
+    this._window.removeEventListener('orientationchange', this.boundEventHandler);
+  }
+
+  _prepareChart() {
+    if (this._chart) {
+      this._chart.destroy();
+    }
+    this._chart = new Chart(this._ctx, {
+      data: {
+        datasets: this._config.dataset(this.data, this.codes),
+        labels: this.labels
+      },
+      options: this._config.options(),
+      type: 'line'
+    });
+    this.show = true;
   }
 }
 
