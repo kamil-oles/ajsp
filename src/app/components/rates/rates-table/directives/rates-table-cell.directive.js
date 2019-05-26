99e6f7671c93eafd432e0d72fc154fe38dd31823
Kamil Olesiński <oles.kamil@gmail.com>
2019-05-26 22:01:50 +0200

    requestAnimationFrame

---
+++ src/app/components/rates/rates-table/directives/rates-table-cell.directive.js
@@ -0,0 +1,22 @@
+class RatesTableCellDirectiveCtrl {
+  /* @ngInject */
+  constructor($element, $scope, eventEmitter) {
+    this._element = $element;
+    this._eventEmitter = eventEmitter;
+    this._scope = $scope;
+  }
+
+  $postLink() {
+    if (!this._element[0].previousElementSibling) {
+      this._scope.passElement(this._eventEmitter(this._element));
+    }
+  }
+}
+
+export class RatesTableCellDirective {
+  constructor() {
+    this.controller = RatesTableCellDirectiveCtrl;
+    this.restrict = 'A';
+    this.scope = { passElement: '&' };
+  }
+}
--- src/app/components/rates/rates-table/rates-table.component.js
+++ src/app/components/rates/rates-table/rates-table.component.js
@@ -1,9 +1,12 @@
 class RatesTableComponentCtrl {
   /* @ngInject */
-  constructor($filter, RatesTableSort) {
-    this.filter = $filter;
+  constructor($element, $filter, RatesTableSort) {
+    this._element = $element;
     this._sortService = RatesTableSort;
+    this.filter = $filter;
   }
+
+  _firstColumn = [];
 
   $onChanges(changes) {
     if (changes.rows.currentValue) {
@@ -21,6 +24,17 @@ class RatesTableComponentCtrl {
     });
   }
 
+  $postLink() {
+    this._rowsArea = this._element.children().children()[1];
+    this._rowsArea.addEventListener('scroll', event => {
+      this._setAnimation(event);
+    });
+  }
+
+  fillArray(element) {
+    this._firstColumn.push(element);
+  }
+
   sort(prop) {
     if (prop === this.sortBy) {
       this.sortDirection = (this.sortDirection === 'ASC' ? 'DESC' : 'ASC');
@@ -33,6 +47,15 @@ class RatesTableComponentCtrl {
   _findColumnToSortBy() {
     const COLUMN = this.columns.find(element => element.sortBy);
     return (COLUMN ? COLUMN.prop : this.columns[0].prop);
+  }
+
+  _setAnimation(event) {
+    function addCss(event) {
+      this._firstColumn.forEach(function setAnimation(element) {
+        element.css('transform', `translate3d(${event.target.scrollLeft}px, 0, 0)`);
+      });
+    }
+    requestAnimationFrame(addCss.bind(this, event));
   }
 }
 
